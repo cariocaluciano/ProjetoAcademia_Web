7bed52f067a46fc6ce1f9334eb90d848abed58f6
Luciano Soares <cariocaluciano1805@gmail.com>
2023-12-16 11:48:12 -0300

    c

--- academia-web/src/App.js
+++ academia-web/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+
         >
           Learn React
         </a>
