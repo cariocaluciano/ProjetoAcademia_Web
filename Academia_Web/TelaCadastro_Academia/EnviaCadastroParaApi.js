5f335882480019d3f9f3c70f0ebb2c9cb1a9f7c8
Luciano Soares <cariocaluciano1805@gmail.com>
2023-12-22 01:27:02 -0300

    g
    g

--- Academia_Web/TelaCadastro_Academia/EnviaCadastroParaApi.js
+++ Academia_Web/TelaCadastro_Academia/EnviaCadastroParaApi.js
@@ -11,15 +11,23 @@ function fnEnviaCadastro() {
     const contato = document.getElementById("contato").value;
     const senha = document.getElementById("senha").value;
 
-    var retornoValidaCnpj = await fnValidaCNPJ(cnpjAcademia).then((resultado) => {
-      resultado;
-    })
+    var retornoValidaCnpj = await fnValidaCNPJ(cnpjAcademia).then(result => result)
+      .catch(error => error);
+
     if (retornoValidaCnpj == "CNPJ inválido") {
       fnTrataRetornoApi(retornoValidaCnpj);
-      return
+      return;
     }
-    
-    // fnVerificaEmail(email)
+
+    var retornoVerificaEmailNoBanco = await fnVerificaEmail(email).then(result => result)
+    .catch(error => error);
+
+    if(retornoVerificaEmailNoBanco == "Email já cadastrado no Banco de dados")
+    {
+      return;
+    }
+
+   //fnEnviaEmailDeVerificacao(email);
 
     fnConsumirAPI(nomeAcademia, cnpjAcademia, nomeUsuario, cpfUsuario, nomeCompleto, email, contato, senha)
 
@@ -29,8 +37,6 @@ function fnEnviaCadastro() {
 
 function fnConsumirAPI(nomeAcademia, cnpjAcademia, nomeUsuario, cpfUsuario, nomeCompleto, email, contato, senha) {
   const apiUrl = 'https://localhost:7263/api/Adiciona/Academia';
-  // const apiUrl = `https://localhost:7263/api/Adiciona/Academia?NomeAcademia=${nomeAcademia}&CnpjAcademia=${cnpjAcademia}&NomeUsuario=${nomeUsuario}&CpfUsuario=${cpfUsuario}&NomeCompleto=${nomeCompleto}&Email=${email}&Contato=${contato}&Senha=${senha}`
-  // Dados a serem enviados no corpo da requisição
   const dados = {
     nomeAcademia: nomeAcademia,
     cnpjAcademia: cnpjAcademia,
@@ -58,7 +64,7 @@ function fnConsumirAPI(nomeAcademia, cnpjAcademia, nomeUsuario, cpfUsuario, nome
     })
     .then(data => {
       var dataConvertidaParaString = JSON.stringify(data)
-      console.log(dataConvertidaParaString)
+      console.log(dataConvertidaParaString + 'CADASTRA NO BANCO')
       fnTrataRetornoApi(dataConvertidaParaString);
     })
     .catch(error => {
@@ -75,7 +81,7 @@ function fnTrataRetornoApi(dataConvertidaParaString) {
     var closeModalBtn = document.getElementById('closeModalBtn');
 
     modalCSS.style.backgroundColor = 'green'
-    paragrafoDoModal.innerHTML = 'Usuário cadastrado com sucesso.'
+    paragrafoDoModal.innerHTML = 'Faça a confirmação do seu Email.</br> Caso não encontre ,Verifique na caixa de Span'
     paragrafoDoModal.style.color = 'white'
     paragrafoDoModal.style.marginLeft = '25px'
     modal.style.margin = '10px 0 0 10px'
@@ -92,9 +98,9 @@ function fnTrataRetornoApi(dataConvertidaParaString) {
       }
     });
 
-    setTimeout(function () {
-      window.location.href = "../TelaLogin_Academia/index.html";
-    }, 3000);
+    // setTimeout(function () {
+    //   window.location.href = "../TelaLogin_Academia/index.html";
+    // }, 3000);
 
   }
   if (dataConvertidaParaString.includes("Email já cadastrado")) {
@@ -170,49 +176,81 @@ function fnTrataRetornoApi(dataConvertidaParaString) {
 }
 
 async function fnValidaCNPJ(cnpj) {
-  return new Promise((resolve, rejected) => {
-    // Remover caracteres não numéricos
-    cnpj = cnpj.replace(/[^\d]/g, '');
+  return new Promise((resolve, reject) => {
+    try {
+      // Remover caracteres não numéricos
+      cnpj = cnpj.replace(/[^\d]/g, '');
 
-    // Verificar se o CNPJ tem 14 dígitos
-    if (cnpj.length !== 14) {
-      rejected("CNPJ inválido");
-    }
+      // Verificar se o CNPJ tem 14 dígitos
+      if (cnpj.length !== 14) {
+        reject("CNPJ inválido");
+      }
 
-    // Verificar se todos os dígitos são iguais (situação inválida)
-    if (/^(\d)\1+$/.test(cnpj)) {
-      rejected("CNPJ inválido");
-    }
+      // Verificar se todos os dígitos são iguais (situação inválida)
+      if (/^(\d)\1+$/.test(cnpj)) {
+        reject("CNPJ inválido");
+      }
 
-    // Calcular o primeiro dígito verificador
-    let soma = 0;
-    let peso = 2;
-    for (let i = 11; i >= 0; i--) {
-      soma += parseInt(cnpj.charAt(i)) * peso;
-      peso = (peso === 9) ? 2 : peso + 1;
-    }
-    let digitoVerificador1 = (11 - (soma % 11)) % 11;
+      // Calcular o primeiro dígito verificador
+      let soma = 0;
+      let peso = 2;
+      for (let i = 11; i >= 0; i--) {
+        soma += parseInt(cnpj.charAt(i)) * peso;
+        peso = (peso === 9) ? 2 : peso + 1;
+      }
+      let digitoVerificador1 = (11 - (soma % 11)) % 11;
 
-    // Verificar se o primeiro dígito verificador está correto
-    if (parseInt(cnpj.charAt(12)) !== digitoVerificador1) {
-      rejected("CNPJ inválido");
-    }
+      // Verificar se o primeiro dígito verificador está correto
+      if (parseInt(cnpj.charAt(12)) !== digitoVerificador1) {
+        reject("CNPJ inválido");
+      }
 
-    // Calcular o segundo dígito verificador
-    soma = 0;
-    peso = 2;
-    for (let i = 12; i >= 0; i--) {
-      soma += parseInt(cnpj.charAt(i)) * peso;
-      peso = (peso === 9) ? 2 : peso + 1;
-    }
-    let digitoVerificador2 = (11 - (soma % 11)) % 11;
+      // Calcular o segundo dígito verificador
+      soma = 0;
+      peso = 2;
+      for (let i = 12; i >= 0; i--) {
+        soma += parseInt(cnpj.charAt(i)) * peso;
+        peso = (peso === 9) ? 2 : peso + 1;
+      }
+      let digitoVerificador2 = (11 - (soma % 11)) % 11;
 
-    // Verificar se o segundo dígito verificador está correto
-    if (parseInt(cnpj.charAt(13)) !== digitoVerificador2) {
-      rejected("CNPJ inválido");
+      // Verificar se o segundo dígito verificador está correto
+      if (parseInt(cnpj.charAt(13)) !== digitoVerificador2) {
+        reject("CNPJ inválido");
+      }
+      // Se todas as verificações passaram, o CNPJ é válido
+      resolve("CNPJ válido");
     }
-    // Se todas as verificações passaram, o CNPJ é válido
-    resolve("CNPJ válido");
+    catch
+    {
+      reject("CNPJ inválido")
+    }
   });
 }
 
+async function fnVerificaEmail(email) {
+  return new Promise((resolve, reject) => {
+    const apiUrl = `https://localhost:7263/api/Adiciona/VerificaEmail?email=${email}}`
+
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Erro na requisição: ${response.status}`);
+        }
+        reject(response.json())
+      })
+      .then(data => {
+        var dataConvertidaParaString = JSON.stringify(data)
+        fnTrataRetornoApi(dataConvertidaParaString);
+        resolve(dataConvertidaParaString);
+      })
+      .catch(error => {
+        reject('Erro ao consumir a API:', error)
+      });
+  });
+}
--- Academia_Web/TelaCadastro_Academia/Valida_Email/ConfirmaValidaEmail.js
+++ Academia_Web/TelaCadastro_Academia/Valida_Email/ConfirmaValidaEmail.js
@@ -7,11 +7,93 @@ function obterParametrosDaURL() {
 
     // Exiba o valor na página
     if (Token) {
-        document.body.innerHTML += `<p>TOKEN = , ${Token}!</p>`;
+        EnviaTokenParaApi(Token);
+
     } else {
         document.body.innerHTML += `<p>Bem-vindo!</p>`;
     }
 }
 
+function EnviaTokenParaApi(Token) {
+    return new Promise((resolve, reject) => {
+        const apiUrl = `https://localhost:7263/api/Adiciona/confirmartoken?Token=${Token}}`
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Erro na requisição: ${response.status}`);
+                }
+                reject(response.json())
+            })
+            .then(data => {
+                var dataConvertidaParaString = JSON.stringify(data)
+                fnTrataRetornoApi(dataConvertidaParaString)
+                setTimeout(function () {
+                    window.location.href = "../TelaLogin_Academia/index.html";
+                }, 3000);
+                resolve(dataConvertidaParaString);
+            })
+            .catch(error => {
+                reject('Erro ao consumir a API:', error)
+            });
+    });
+}
+
+function fnTrataRetornoApi(dataConvertidaParaString) {
+    if (dataConvertidaParaString.includes("Tabela Atualizada.")) {
+      var paragrafoDoModal = document.getElementById('paragrafoModalCadAcademia')
+      var modal = document.getElementById('modal');
+      var modalCSS = document.querySelector('.modal-content')
+      var closeModalBtn = document.getElementById('closeModalBtn');
+  
+      modalCSS.style.backgroundColor = 'green'
+      paragrafoDoModal.innerHTML = 'Email validado  </br>  Redirecionando para o login...'
+      paragrafoDoModal.style.color = 'white'
+      paragrafoDoModal.style.marginLeft = '25px'
+      modal.style.margin = '10px 0 0 10px'
+  
+      modal.style.display = 'block';
+      modal.style.width = '30%'
+      closeModalBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+      });
+  
+      window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+          modal.style.display = 'none';
+        }
+      });
+    }
+  else
+  {
+    var paragrafoDoModal = document.getElementById('paragrafoModalCadAcademia')
+    var modal = document.getElementById('modal');
+    var modalCSS = document.querySelector('.modal-content')
+    var closeModalBtn = document.getElementById('closeModalBtn');
+
+    modalCSS.style.backgroundColor = 'green'
+    paragrafoDoModal.innerHTML = 'Token inválido </br> tente efetuar o cadastro novamente.'
+    paragrafoDoModal.style.color = 'white'
+    paragrafoDoModal.style.marginLeft = '25px'
+    modal.style.margin = '10px 0 0 10px'
+
+    modal.style.display = 'block';
+    modal.style.width = '30%'
+    closeModalBtn.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  }
+}
 // Chame a função quando a página carregar
 window.onload = obterParametrosDaURL;
