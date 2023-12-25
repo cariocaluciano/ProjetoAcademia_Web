e3ab84318b7d8113d239448601f58a6493de7fd7
Luciano Soares <cariocaluciano1805@gmail.com>
2023-12-25 20:49:47 -0300

    d

---
+++ Academia_Web/TelaPrincipal/TelaPrincipal.js
@@ -0,0 +1,53 @@
+const paragrafoDoCabecalhoQueRecebeIdAcademia = document.getElementById('NomeUsuarioEIdAcademia');
+const idAcademia = obterParametrosDaURL();
+
+
+
+
+function obterParametrosDaURL() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const IdAcademia = urlParams.get('Id');
+    return IdAcademia;
+}
+
+function fnRetornaInformacoesDaAcademia() {
+    const apiUrl = `https://localhost:7263/api/DadosAcademia/GetAcademia?IdAcademia=${idAcademia}`
+
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            var dataConvertidaParaString = JSON.stringify(data)
+            var objetoJSON = JSON.parse(JSON.parse(dataConvertidaParaString).mensagem);
+
+            var nome = objetoJSON.Nome;
+            var idAcademia = objetoJSON.IdAcademia;
+
+            fnInsereNoHtmlOsDadosDaAcademia(idAcademia,nome)
+        })
+        .catch(error => {
+            console.error('Erro ao consumir a API:', error);
+        });
+
+}
+
+function fnInsereNoHtmlOsDadosDaAcademia(idAcademia,nome){
+
+    paragrafoDoCabecalhoQueRecebeIdAcademia.innerText = `${nome} Id: ${idAcademia}` ;
+}
+
+
+window.onload = obterParametrosDaURL;
+window.onload = fnRetornaInformacoesDaAcademia;
