76197b9ecb244570a0ebd8cf983f059f9c5af9d7
Luciano Soares <cariocaluciano1805@gmail.com>
2024-01-15 22:08:03 -0300

    b

--- Academia_Web/JS/Cabecalho.js
+++ Academia_Web/JS/Cabecalho.js
@@ -46,7 +46,7 @@ var HtmlCabecalhoDireito = `  <nav class="navbar navbar-dark bg-dark fixed-top">
             </form>
         </div>
         <div class="offcanvas-footer">
-            <button type="button"class="btn icon-link icon-link-hover" style = "color: white;" onclick = "fnLogOf(this)">Sair <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
+            <button type="button"class="btn icon-link icon-link-hover" style = "color: white;" data-bs-toggle="modal" data-bs-target="#ModalLogOf" >Sair <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
             <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3zm1 13h8V2H4z"/>
             <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0"/>
           </svg></button>
--- Academia_Web/JS/LogOf.js
+++ Academia_Web/JS/LogOf.js
@@ -1,3 +1,63 @@
-function fnLogOf(IdDaAcademia){
 
+function fnLogOf() {
+    const NomeUsuarioEIdAcademia = document.getElementById("NomeUsuarioEIdAcademia").textContent;
+    const id = NomeUsuarioEIdAcademia.match(/\d+/g).map(Number);
+    const idAcademia = id[0];
+
+    console.log(idAcademia)
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const Token = urlParams.get('Tk');
+
+    console.log(Token)
+    fnConsomeApiLogOf(Token, idAcademia)
+
+}
+
+function fnConsomeApiLogOf(Token, idAcademia) {
+
+    const url = `https://localhost:7263/Api/LogOf`
+
+    const dados =
+    {
+        token: Token,
+        idAcademia: idAcademia
+    }
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            var dataConvertidaParaString = JSON.stringify(data);
+            fntrataretornoApi(dataConvertidaParaString);
+        })
+        .catch(error => {
+            console.Error('Erro ao consumir a API :', error);
+        })
+}
+
+function fntrataretornoApi(dataConvertidaParaString){
+if(dataConvertidaParaString.includes('Sucesso'))
+{
+
+    setTimeout(fnRedirecionaParatelaDeLogin,1000)
+}
+
+
+}
+
+function fnRedirecionaParatelaDeLogin(){
+    window.location.href = 'http://127.0.0.1:5500/Academia_Web/TelaLogin_Academia/index.html';
 }
